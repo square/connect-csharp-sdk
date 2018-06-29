bc307d0254906338245330c04891bc8f8ac9668a
Shao-Fu Sung <ssung@squareup.com>
2018-06-29 13:22:24 -0700

    test

--- src/Square.Connect/Client/ApiClient.cs
+++ src/Square.Connect/Client/ApiClient.cs
@@ -132,7 +132,7 @@ namespace Square.Connect.Client
             // add file parameter, if any
             foreach(var param in fileParams)
             {
-                request.AddFile(param.Value.Name, param.Value.Writer, param.Value.FileName, param.Value.ContentType);
+                request.AddFile(param.Value.Name, param.Value.Writer, param.Value.FileName, param.Value.ContentLength, param.Value.ContentType);
             }
 
             if (postBody != null) // http body (model or byte[]) parameter
