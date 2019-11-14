da4923323c6975e0b030c722a2cab33af1994612
Shao-Fu Sung <20245766+shaofu88@users.noreply.github.com>
2019-11-14 09:05:50 -0800

    Publish 2.24.0 (#144)

--- src/Square.Connect/Model/AdditionalRecipient.cs
+++ src/Square.Connect/Model/AdditionalRecipient.cs
@@ -132,7 +132,6 @@ namespace Square.Connect.Model
             // credit: http://stackoverflow.com/a/10454552/677735
             return this.Equals(obj as AdditionalRecipient);
         }
-
         /// <summary>
         /// Returns true if AdditionalRecipient instances are equal
         /// </summary>
